7f0e4518b8a303884b1fc9843d47e7e928fc999f
samuelsadev <samuelsa.dev@gmail.com>
2024-07-15 16:34:17 -0300

    Criando UsuarioRepository e packege security com a classe UserDetailsImpl

---
+++ src/main/java/com/generation/blogPessoal/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.generation.blogPessoal.repository;
+
+import com.generation.blogPessoal.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByUsuario(String usuario);
+}
