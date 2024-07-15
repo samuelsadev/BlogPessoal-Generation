60a82d6679cd33e570a8687262ab23c90dde55cb
samuelsadev <samuelsa.dev@gmail.com>
2024-07-15 20:23:02 -0300

    UserDetailsServiceImpl teste

--- src/main/java/com/generation/blogPessoal/security/UserDetailsServiceImpl.java
+++ src/main/java/com/generation/blogPessoal/security/UserDetailsServiceImpl.java
@@ -4,12 +4,14 @@ import com.generation.blogPessoal.model.Usuario;
 import com.generation.blogPessoal.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Collections;
 import java.util.Optional;
 
 @Service
@@ -18,14 +20,31 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//
+//        Optional<Usuario> usuario = usuarioRepository.findByUsuario(username);
+//
+//        if (usuario.isEmpty())
+//            return new UserDetailsImpl(usuario.get());
+//        else
+//            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+//    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Usuario> usuarioOptional = usuarioRepository.findByUsuario(username);
 
-        Optional<Usuario> usuario = usuarioRepository.findByUsuario(username);
+        if (usuarioOptional.isEmpty()) {
+            throw new UsernameNotFoundException("Usuário não encontrado com o username: " + username);
+        }
 
-        if (usuario.isEmpty())
-            return new UserDetailsImpl(usuario.get());
-        else
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        Usuario usuario = usuarioOptional.get();
+
+        return User.builder()
+                .username(usuario.getUsuario())
+                .password(usuario.getSenha())
+                .authorities(Collections.emptyList())
+                .build();
     }
 }
