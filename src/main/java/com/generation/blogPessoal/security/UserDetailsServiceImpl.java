0ef652897e0d7ed03641d7ce48aa1f3ef1381468
samuelsadev <samuelsa.dev@gmail.com>
2024-07-17 10:38:33 -0300

    correção UserDetailsServiceImpl

--- src/main/java/com/generation/blogPessoal/security/UserDetailsServiceImpl.java
+++ src/main/java/com/generation/blogPessoal/security/UserDetailsServiceImpl.java
@@ -20,31 +20,14 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
-//    @Override
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//
-//        Optional<Usuario> usuario = usuarioRepository.findByUsuario(username);
-//
-//        if (usuario.isEmpty())
-//            return new UserDetailsImpl(usuario.get());
-//        else
-//            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
-//    }
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<Usuario> usuarioOptional = usuarioRepository.findByUsuario(username);
 
-        if (usuarioOptional.isEmpty()) {
-            throw new UsernameNotFoundException("Usuário não encontrado com o username: " + username);
-        }
+        Optional<Usuario> usuario = usuarioRepository.findByUsuario(username);
 
-        Usuario usuario = usuarioOptional.get();
-
-        return User.builder()
-                .username(usuario.getUsuario())
-                .password(usuario.getSenha())
-                .authorities(Collections.emptyList())
-                .build();
+        if (usuario.isPresent())
+            return new UserDetailsImpl(usuario.get());
+        else
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
     }
 }
