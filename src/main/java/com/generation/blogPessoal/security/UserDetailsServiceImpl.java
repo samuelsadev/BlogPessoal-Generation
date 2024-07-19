c17657baed84d2f27225f5c2898cd3bb0a5f7106
samuelsadev <samuelsa.dev@gmail.com>
2024-07-19 10:14:49 -0300

    Criando teste

--- src/main/java/com/generation/blogPessoal/security/UserDetailsServiceImpl.java
+++ src/main/java/com/generation/blogPessoal/security/UserDetailsServiceImpl.java
@@ -4,14 +4,12 @@ import com.generation.blogPessoal.model.Usuario;
 import com.generation.blogPessoal.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Collections;
 import java.util.Optional;
 
 @Service
